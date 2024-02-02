bd946959c5134dd6f66a5ba9cbe7aa430ff721c9
PandasAreBears <me@me.com>
2024-02-02 19:14:13 +0000

    Run the test container with SYS_ADMIN capability.
    This is a neccesity when using apfs-fuse as pointed out in https://github.com/s3fs-fuse/s3fs-fuse/issues/647

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 export DOCKER_BUILDKIT=1
 docker build -t flatten-ipsw .
 docker build -f Dockerfile.test -t flatten-ipsw-test .
-docker run --rm flatten-ipsw-test
+docker run --rm --cap-add SYS_ADMIN --device /dev/fuse flatten-ipsw-test
