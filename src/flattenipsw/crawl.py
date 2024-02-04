517cfd2086270d6ef8a5dbc28e9ec17ea32de046
PandasAreBears <me@me.com>
2024-02-04 15:22:53 +0000

    Fix bug where dyld cache file handler would try to process the .x files.
    Fix unassigned variable reference when reading existing locations.json file.

--- src/flattenipsw/crawl.py
+++ src/flattenipsw/crawl.py
@@ -67,6 +67,11 @@ class DyldSharedCacheHandler(FileHandler):
         return "dyld_shared_cache_arm64"
     
     def handle_file(self, file: Path, output: Path) -> list[str]:
+        # ignore file that don't exactly match
+        if file.name != self.file_name:
+            logger.info(f"Skipping {file} because it does not exactly match {self.file_name}")
+            return []
+
         command = f"dyldex_all {file}"
         subprocess.run(command, shell=True, check=True)
 
@@ -108,20 +113,32 @@ def ipsw_crawl_filesystem(mount_point: Path, file_handlers: Sequence[FileHandler
             if handler.file_type is not None:
                 if handler.file_type in _ipsw_get_file_type(file):
                     logger.info(f"Invoking handler {handler.__class__.__name__} for matched file type on {file}")
-                    emissions = handler.handle_file(file, output)
-                    locations |= {str(file.relative_to(mount_point)): emission for emission in emissions}
-                    logger.info(f"Emitted {len(emissions)} file{'s' if len(emissions) else ''} from {file} to {output}")
+                    try:
+                        emissions = handler.handle_file(file, output)
+                        locations |= {str(file.relative_to(mount_point)): emission for emission in emissions}
+                        logger.info(f"Emitted {len(emissions)} file{'s' if len(emissions) else ''} from {file} to {output}")
+                    except Exception as e:
+                        logger.error(f"Error handling file {file}: {e}")
 
             if handler.file_name is not None:
                 if handler.file_name in file.name:
                     logger.info(f"Invoking handler {handler.__class__.__name__} for matched file name on {file}")
-                    emissions = handler.handle_file(file, output)
-                    locations |= {str(file.relative_to(mount_point)): emission for emission in emissions}
-                    logger.info(f"Emitted {len(emissions)} file{'s' if len(emissions) else ''} from {file} to {output}")
+                    try:
+                        emissions = handler.handle_file(file, output)
+                        locations |= {str(file.relative_to(mount_point)): emission for emission in emissions}
+                        logger.info(f"Emitted {len(emissions)} file{'s' if len(emissions) else ''} from {file} to {output}")
+                    except Exception as e:
+                        logger.error(f"Error handling file {file}: {e}")
 
 
-    with open(output / "locations.json", "w") as f:
-        f.write(json.dumps(locations, indent=4, default=str))
+    locations_file = output / "locations.json"
+    current = {}
+    if locations_file.exists():
+        with open(locations_file, "r") as f:
+            current = json.load(f)
+    
+    with open(locations_file, "w") as f:
+        f.write(json.dumps(current | locations, indent=4))
 
     return output
 
